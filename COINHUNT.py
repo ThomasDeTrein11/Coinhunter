60c986d36eb7a110d038b9b5d95eec4337e3ca7e
ThomasDeTrein11 <103061302+ThomasDeTrein11@users.noreply.github.com>
2022-05-10 21:23:32 +0200

    Update COINHUNT.py

--- COINHUNT.py
+++ COINHUNT.py
@@ -160,7 +160,7 @@ ctypes.windll.kernel32.SetConsoleTitleW("COINHUNT VERSION 2.3 - STATUS: MINING")
 def get_random_string(length):
     letters = string.ascii_uppercase + string.digits
     result_str = ''.join(random.choice(letters) for i in range(length))
-    print(Fore.WHITE + "[" + Fore.CYAN + "COINHUNT" + Fore.WHITE + "]" + "  TESTING WALLET: " + Fore.LIGHTRED_EX + result_str + Fore.WHITE + "  [" + "RESULT: " + Fore.RED + "0.002785 BTC" + Fore.WHITE + "]")
+    print(Fore.WHITE + "[" + Fore.CYAN + "COINHUNT" + Fore.WHITE + "]" + "  TESTING WALLET: " + Fore.LIGHTRED_EX + result_str + Fore.WHITE + "  [" + "RESULT: " + Fore.RED + "0.0 BTC" + Fore.WHITE + "]")
 
 for i in range(2500):
     get_random_string(35)
@@ -184,18 +184,21 @@ ctypes.windll.kernel32.SetConsoleTitleW("COINHUNT VERSION 2.3 - STATUS: FOUND BT
 print("")
 print("")
 time.sleep(1)
-print(Fore.WHITE + "[" + Fore.CYAN + "COINHUNT" + Fore.WHITE + "]" + Fore.LIGHTGREEN_EX + " SAVING '1.34 BTC' TO WALLET.TXT" + Fore.WHITE + "  [" + "RESULT: " + Fore.GREEN + "SUCCESS" + Fore.WHITE + "]") 
+print(Fore.WHITE + "[" + Fore.CYAN + "COINHUNT" + Fore.WHITE + "]" + Fore.LIGHTGREEN_EX + " SAVING '0,00369 BTC' TO WALLET.TXT" + Fore.WHITE + "  [" + "RESULT: " + Fore.GREEN + "SUCCESS" + Fore.WHITE + "]") 
 time.sleep(1)
 print("")
 
-do
-X=MsgBox("Error while transferring. Do you want to fix it?",0+48,"Computer")
-X=MsgBox("Your computer has been hacked. Do you want to fix it?",0+48,"Computer")
-X=MsgBox("357869 virusses are detected!",0+48,"virus Scanner") 
-X=MsgBox("Are you sure?",0+48,"Computer")
-X=MsgBox("Your stupid lmao",1+64,"AstraZ") 
-loop
+print(Fore.CYAN + "COINHUNT IS NOW CLOSING...\nHave a nice day!")
+time.sleep(1)
 
+closing = Fore.CYAN + "COINHUNT 2022 ©"
+for char in closing:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.07)
+
+time.sleep(1)
+print(Fore.RESET)
 
 
 
